47d2977906c2cc6e7dc6552fd387079075da58c2
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-10-09 21:48:50 +0300

    deploying somewhere else

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://twodo.vercel.app/login'], // Your frontend URL
+  origin: ['https://twodo.vercel.app/', 'http://localhost:5173/'], // Your frontend URL
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
