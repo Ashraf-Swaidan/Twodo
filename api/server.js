2030f934f5f63d095260b70759ef85dfca000cb6
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-10-09 21:53:28 +0300

    This should make it work.

--- api/server.js
+++ api/server.js
@@ -13,13 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors({
-  origin: ['https://twodo.vercel.app/', 'http://localhost:5173/'], // Your frontend URL
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 app.use('/api/auth', authRoutes);
