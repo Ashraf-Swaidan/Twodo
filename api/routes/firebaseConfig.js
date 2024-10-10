e7d4d365177495baed90fa56537d3f5811242f02
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-10-10 14:11:08 +0300

    Working at understand how Render hosting service handles Secret files

--- api/routes/firebaseConfig.js
+++ api/routes/firebaseConfig.js
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
 import fs from 'fs';
+import path from 'path';
 
-// Path to the Firebase secret file from Render's secret management
-const serviceAccountPath = process.env.RENDER_SECRET_FILE_FirebaseServiceAccountKey;
-console.log('Service Account Path:', process.env.RENDER_SECRET_FILE_FirebaseServiceAccountKey);
+// Path to the Firebase service account secret file
+const serviceAccountPath = '/etc/secrets/FirebaseServiceAccountKey.json';
 
 // Read the Firebase service account key from the file
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
@@ -11,7 +11,7 @@ const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Ensure you set this in Render environment
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Ensure this is set in Render environment
 });
 
 // Export Firebase bucket for file operations
